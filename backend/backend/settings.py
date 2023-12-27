62150c9516ac3b62d456f89b82f891386b896821
Viktor Ledenev <bigbagbom@yandex.ru>
2023-12-27 06:57:33 +0300

    add test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'django_db'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
